eb4da2e4f3357af7306ae2b6c44609a3339df187
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-06-08 19:31:57 +0300

    start

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -6,43 +6,53 @@ import "./mdx.css";
 import { ReportView } from "./view";
 import { Redis } from "@upstash/redis";
 
-export const revalidate = 60;
+// export const revalidate = 60;
+export const revalidate = 0;
 
-type Props = {
-	params: {
-		slug: string;
-	};
+type ProductSingleProps = {
+  params: {
+    slug: string;
+  };
 };
 
 const redis = Redis.fromEnv();
 
-export async function generateStaticParams(): Promise<Props["params"][]> {
-	return allProjects
-		.filter((p) => p.published)
-		.map((p) => ({
-			slug: p.slug,
-		}));
-}
+// export async function generateStaticParams(): Promise<ProductSingleProps["params"][]> {
+//   return allProjects
+//     .filter((p) => p.published)
+//     .map((p) => ({
+//       slug: p.slug,
+//     }));
+// }
 
-export default async function PostPage({ params }: Props) {
-	const slug = params?.slug;
-	const project = allProjects.find((project) => project.slug === slug);
+export const getProjectBySlug = async (slug: string) => {
+  const project = allProjects.find(
+    (project) => project.slug === slug && project.published
+  );
 
-	if (!project) {
-		notFound();
-	}
+  return project;
+};
 
-	const views =
-		(await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
+export default async function PostPage({ params }: ProductSingleProps) {
+  const slug = params?.slug;
+  const project = await getProjectBySlug(slug);
+  // const project = allProjects.find((project) => project.slug === slug);
 
-	return (
-		<div className="bg-zinc-50 min-h-screen">
-			<Header project={project} views={views} />
-			<ReportView slug={project.slug} />
+  if (!project) {
+    notFound();
+  }
 
-			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
-				<Mdx code={project.body.code} />
-			</article>
-		</div>
-	);
+  const views =
+    (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
+
+  return (
+    <div className="bg-zinc-50 min-h-screen">
+      <Header project={project} views={views} />
+      <ReportView slug={project.slug} />
+
+      <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
+        <Mdx code={project.body.code} />
+      </article>
+    </div>
+  );
 }
